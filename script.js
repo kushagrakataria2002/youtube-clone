4ab01f6fd826d2add06fb382d57070c1717f0cc6
kushagra kataria <kushagrakataria19@gmail.com>
2023-09-06 16:47:28 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const sideBarBtn = document.querySelector(".toogler"); 
+const sideBar = document.querySelector('.unorderedList'); 
+const searchIcon = document.querySelector('.searchLogo'); 
+const inputField = document.querySelector('#default-input'); 
+const refrenceContainer = document.querySelector('.template'); 
+const cards = document.querySelector('.cards'); 
+
+const apiCallOnSearch = () =>{
+    const apiObject = new XMLHttpRequest(); 
+    apiObject.open('GET', `https://youtube-v31.p.rapidapi.com/search?q=${inputField.value}%20&part=snippet%2Cid&regionCode=in&maxResults=50&order=relevance`);
+    
+    apiObject.setRequestHeader('X-RapidAPI-Key', '00df45cbdfmshf650d2235a6eceap1e9556jsn67885b5ff73c');
+    apiObject.setRequestHeader('X-RapidAPI-Host', 'youtube-v31.p.rapidapi.com');
+
+    apiObject.onload = () =>{
+        const data = JSON.parse(apiObject.response); 
+        console.log(data); 
+        let count = 0; 
+        while(count <= 49){
+            let valueToBeInserted = `
+            <div class="cards hover:cursor-pointer lg:w-2/6 md:w-1/2 sm:w-3/5 p-4 w-full">
+                <a href = "player.html"class="block relative h-48 rounded overflow-hidden">
+                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src='${data.items[count].snippet.thumbnails.high.url}'>
+                </a>
+                <div class="mt-4">
+                    <h2 class="text-gray-900 title-font text-lg font-medium">${data.items[count].snippet.title}</h2>
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">${data.items[count].snippet.channelTitle}</h3>
+                </div>
+            </div>
+            `; 
+            refrenceContainer.insertAdjacentHTML('afterbegin', valueToBeInserted); 
+            count = count + 1; 
+        }
+    }; 
+    refrenceContainer.innerHTML = ``; 
+    apiObject.send(); 
+}; 
+
+
+// sideBarBtn.addEventListener("click", () =>{
+//     sideBar.classList.toggle("hidden"); 
+// });
+
+inputField.addEventListener("keypress", (event) =>{
+    if(event.key === "Enter"){
+        apiCallOnSearch(); 
+    }
+}); 
+
+searchIcon.addEventListener('click', () =>{
+    apiCallOnSearch(); 
+}); 
+
+cards.addEventListener('click', () =>{
+    alert('hello'); 
+}); 
---
+++ script2.js
@@ -0,0 +1,35 @@
+const refrenceContainerLoader = document.querySelector('.template'); 
+
+const apiCallOnLoad = () =>{
+    const apiObjectLoad = new XMLHttpRequest(); 
+    apiObjectLoad.open('GET', `https://youtube-v31.p.rapidapi.com/search?q=c++programming%20&part=snippet%2Cid&regionCode=in&maxResults=50&order=relevance`);
+    
+    apiObjectLoad.setRequestHeader('X-RapidAPI-Key','00df45cbdfmshf650d2235a6eceap1e9556jsn67885b5ff73c');
+    apiObjectLoad.setRequestHeader('X-RapidAPI-Host', 'youtube-v31.p.rapidapi.com');
+
+    apiObjectLoad.onload = () =>{
+        const loadData = JSON.parse(apiObjectLoad.response); 
+        // console.log(loadData); 
+        let countLoad = 0; 
+        while(countLoad <= 49){
+            let valueToBeInsertedLoad = `
+            <div class="hover:cursor-pointer lg:w-2/6 md:w-1/2 sm:w-1/2 p-4 w-full">
+                <a href = "player.html"class="block relative h-48 rounded overflow-hidden">
+                    <img alt="ecommerce" class="object-cover object-center w-full h-full block" src='${loadData.items[countLoad].snippet.thumbnails.high.url}'>
+                </a>
+                <div class="mt-4">
+                    <h2 class="text-gray-900 title-font text-lg font-medium">${loadData.items[countLoad].snippet.title}</h2>
+                    <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">${loadData.items[countLoad].snippet.channelTitle}</h3>
+                </div>
+            </div>
+            `; 
+            refrenceContainerLoader.insertAdjacentHTML('afterbegin', valueToBeInsertedLoad); 
+            countLoad = countLoad + 1; 
+        }
+    }; 
+    apiObjectLoad.send(); 
+};
+
+window.addEventListener('load', () =>{
+    apiCallOnLoad(); 
+}); 
